2ff279679e02c6902988b48d62a23be86f92667f
Ben Folan <60395517+ocualaib@users.noreply.github.com>
2021-04-09 19:01:58 +0100

    Update StatsController.cs

--- src/StatsController.cs
+++ src/StatsController.cs
@@ -13,8 +13,8 @@ namespace Server.Controllers
         [HttpGet]
         public ActionResult <string> getDoorStats()
         {
-            JObject data = JObject.Parse(System.IO.File.ReadAllText("\\data.json"));
-            string dataString = data.ToString();
+            //JObject data = JObject.Parse(System.IO.File.ReadAllText("\\data.json"));
+            //string dataString = data.ToString();
             return Ok("dataString");
         }
     }
